b4d0f7df8a4f326c30cd82d27f3fe545b2dcb2f0
Amine2k0 <115274386+Amine2k0@users.noreply.github.com>
2026-09-25 14:29:48 +0000

    feat(backend): generate spring boot 4 project skeleton

---
+++ backend/src/test/java/io/github/amine2k0/portfolio/PortfolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.amine2k0.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
